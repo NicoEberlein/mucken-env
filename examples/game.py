8e8edc6d40a489c6874ca233e47abfa98ee66190
NicoEberlein <nico@eberln.de>
2025-11-29 02:11:26 +0100

    docs: add example agent and game loop

---
+++ examples/game.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from examples.random_agent import Agent
+from mucken_env import MuckenEnv
+
+if __name__ == '__main__':
+
+    env = MuckenEnv(render_mode="human")
+
+    agents = {
+        "player_0": Agent(),
+        "player_1": Agent(),
+        "player_2": Agent(),
+        "player_3": Agent(),
+    }
+
+    rewards_collected = { agent: 0.0 for agent in agents }
+
+    env.reset()
+
+    for agent_id in env.agent_iter():
+
+        env.render()
+
+        observation, reward, termination, truncation, info = env.last()
+
+        rewards_collected[agent_id] += reward
+
+        action = None
+
+        if not (termination or truncation):
+            action = agents[agent_id].choose_action(observation)
+
+        env.step(action)
+
+    print("rewards_collected", rewards_collected)
--- examples/random_agent.py
+++ examples/random_agent.py
@@ -1,5 +1,3 @@
-import time
-
 import numpy as np
 
 class Agent:
