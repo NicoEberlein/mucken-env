c095e17f0bf6bd8e4ef2c3e3f229da3b170acf88
NicoEberlein <nico@eberln.de>
2025-12-29 20:34:09 +0100

    test: add test case when player tries to play card not on his hand

--- tests/testenv.py
+++ tests/testenv.py
@@ -8,7 +8,7 @@ from mucken_env.cards.enums import Color, Face
 
 def test_pettingzoo_api():
     env = MuckenEnv()
-    api_test(env, num_cycles=10, verbose_progress=False)
+    api_test(env, num_cycles=1000, verbose_progress=False)
 
 def test_seeding():
     env0 = MuckenEnv()
@@ -36,5 +36,34 @@ def test_invalid_action():
 
     _, _, termination, truncation, _ = env.last()
     assert termination or truncation
+
+def test_missing_card_player_0():
+    env = MuckenEnv()
+    env.reset(seed=41)
+
+    env.step(get_unique_id(Card(Color.BLATT, Face.ZEHN)))
+
+    assert env.rewards['player_0'] == -1
+
+    _, _, termination, truncation, _ = env.last()
+
+    assert termination
+    
+def test_missing_card_player_1():
+    env = MuckenEnv()
+    env.reset(seed=41)
+
+    env.step(get_unique_id(Card(Color.BLATT, Face.KOENIG)))
+
+    _, _, termination, truncation, _ = env.last()
+
+    assert not (truncation or termination)
+
+    # invalid action
+    env.step(get_unique_id(Card(Color.SCHELLE, Face.NEUN)))
+    assert env.rewards['player_1'] == -1
+
+    _, _, termination, truncation, _ = env.last()
+    assert termination
 
 
